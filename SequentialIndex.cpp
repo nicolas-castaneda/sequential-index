8c70fa3ca0e0257841554ba9b6cf250936751251
nicolas-castaneda <nicolas.castaneda@utec.edu.pe>
2023-09-02 18:58:14 -0500

    Add - BinarySearchResponse struct base

--- SequentialIndex.cpp
+++ SequentialIndex.cpp
@@ -3,6 +3,74 @@
 
 #include "SequentialIndex.h"
 
+/*
+    Read and write a record from a sequential File
+*/
+template<typename FileType = std::fstream>
+void SequentialIndex::writeHeader(FileType& indexFile, physical_pos next_pos, file_pos next_file){
+    try {
+        indexFile.seekp(0, std::ios::beg);
+        SequentialIndexHeader sih(next_pos, next_file);
+        indexFile.write(reinterpret_cast<char*> (&sih), sizeof(SequentialIndexHeader));
+    } catch (...) {
+        throw std::runtime_error("Couldn't write header");
+    }
+}
+
+template<typename FileType = std::fstream>
+void SequentialIndex::readHeader(FileType& indexFile, SequentialIndexHeader& sih){
+    try {
+        indexFile.seekp(0, std::ios::beg);
+        indexFile.read(reinterpret_cast<char*> (&sih), sizeof(SequentialIndexHeader));
+    } catch (...) {
+        throw std::runtime_error("Couldn't read header");
+    }
+    
+}
+
+template<typename FileType = std::fstream>
+void SequentialIndex::readRecord(FileType& file, SequentialIndexRecord& sir){
+    try {
+        file.read(reinterpret_cast<char*> (&sir), sizeof(SequentialIndexRecord));
+    } catch (...) {
+        throw std::runtime_error("Couldn't read record");
+    }
+}
+
+/*
+    Helper functions 
+*/
+void SequentialIndex::createFile(){
+        std::ofstream indexFile(this->indexFilename, std::ios::app | std::ios::binary);
+        if(!indexFile.is_open()) throw std::runtime_error("Couldn't create indexFile");
+        writeHeader<decltype(indexFile)> (indexFile, -1, INDEXFILE);
+        indexFile.close();
+
+        std::ofstream auxFile(this->auxFilename, std::ios::app | std::ios::binary);
+        if(!auxFile.is_open())  throw std::runtime_error("Couldn't create auxFile"); 
+        auxFile.close();
+
+        std::ofstream duplicatesFile(this->duplicatesFilename, std::ios::app | std::ios::binary);
+        if(!duplicatesFile.is_open()) throw std::runtime_error("Couldn't create duplicatesFile");
+        duplicatesFile.close();
+    }
+
+bool SequentialIndex::fileExists(){
+        std::ifstream file(this->indexFilename);
+        bool exists = file.good();
+        file.close();
+        return exists;
+}
+
+/*
+    Query functions
+*/
+
+
+
+/*
+    Print files sequentially
+*/
 void SequentialIndex::printIndexFile() {
     std::fstream indexFile(this->indexFilename, std::ios::in | std::ios::out | std::ios::binary);
     SequentialIndexHeader sih;
